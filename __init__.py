09975352cc28a38fa82419fd09d1f51fb69ee146
Brendon <425026+brendon-codes@users.noreply.github.com>
2012-09-02 12:15:08 -0700

    Adding

---
+++ __init__.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+"""
+Goodies for helping with iterables
+"""
+
+import math
+from collections import Iterable
+from itertools import izip
+from itertools import chain
+
+
+def n_chunks(r, n):
+    """
+    Splits an iterable in n chunks.
+    Accepts dicts, lists, tuples, and
+    some types of generators
+
+    Author: Brendon Crawford
+    """
+    s = int(math.ceil(float(len(r)) / float(n)))
+    return chunks(r, s)
+
+
+def chunks_of_n(r, n):
+    """
+    Alias for `chunks`
+
+    Author: Brendon Crawford
+    """
+    return chunks(r, n)
+
+
+def _chunks(r, n):
+    """
+    Splits an iterable into chunks of n.
+    Accepts dicts, lists, tuples, and some types
+    of generators.
+
+    Author: Brendon Crawford
+    """
+    ##
+    ## Dict
+    ##
+    if isinstance(r, dict):
+        keys, vals = zip(*r.iteritems())
+        out = (
+            dict((keys[ii], vals[ii]) for ii in xrange(i, i + n) \
+                  if ii < len(r)) \
+            for i in xrange(0, len(r), n)
+        )
+    ##
+    ## List or anything that has a length and is sliceable.
+    ## This most likely includes a Django QuerySet result
+    ##
+    elif isinstance(r, Iterable) and \
+            hasattr(r, '__len__') and \
+            hasattr(r, '__getitem__'):
+        out = (r[i:i + n] for i in xrange(0, len(r), n))
+    ##
+    ## Some other generator that can be converted
+    ## to a lsit
+    ##
+    elif isinstance(r, Iterable):
+        ri = list(r)
+        out = (ri[i:i + n] for i in xrange(0, len(ri), n))
+    ##
+    ## Unknown
+    ##
+    else:
+        raise Exception('Cannot chunkify!!')
+    return out
